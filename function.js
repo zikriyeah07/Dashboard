dbada643b8f8c4d70f0f4b8034b8a213090ce899
zikriyeah07 <zikriyakhan64642@gmail.com>
2025-02-17 03:46:04 +0500

    updated

---
+++ function.js
@@ -0,0 +1,12 @@
+// window.addEventListener('wrapper', function() {
+//     // Hide the loader
+//     document.getElementById('wrapper').style.display = 'none';
+
+//     // Show the content
+//     document.getElementById('main').style.display = 'block';
+// });
+
+// let num1 = prompt("What's your first name?");
+// if (num1 != "zikriya") {
+//   alert("No match");
+// }
